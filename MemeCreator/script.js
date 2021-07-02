a3601fc26749d8904181940bbde6d087f83a9be8
CarlosCapili <capilc1@mcmaster.ca>
2021-07-02 13:11:15 -0400

    Finished

--- MemeCreator/script.js
+++ MemeCreator/script.js
@@ -4,3 +4,9 @@ function update_text() {
 
 	meme_text.innerHTML = text.value;
 }
+
+function update_image() {
+	var img = document.querySelector('img');
+	var file = document.querySelector('input[type=file]').files[0];
+	img.src = window.URL.createObjectURL(file);
+}
