0bc1187265d3ae8e05e1fd186e67325efab86341
CarlosCapili <capilc1@mcmaster.ca>
2021-07-08 21:35:16 -0400

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,43 +1,53 @@
 var button = document.querySelector(".calc-keypad");
 var display = document.getElementById("display");
-var numPressed = [];
+var btnPressed = [];
+var opPressed = [];
 
 button.addEventListener('click', btn_clicked);
 
 function btn_clicked() {
 	var id = event.target.id;
-
-	if (id == "equal"){
+	 
+	if (id == "equal") {
 		evaluate();
 	}
-	else if (id == "AC"){
+	else if (id == "AC") {
 		clearDisplay();
 	}
-	else {
-		numPressed.push(id);
-		console.log(numPressed);
-		updateDisplay(id);
+	else if (id == "+" || id == "-" || id == "*" || id == "/") {
+		opPressed.push(btnPressed.join("") + id);
+		btnPressed = []; 
+		console.log(opPressed);
+		updateDisplay();
 	}
+	else {
+		btnPressed.push(id);
+		console.log(btnPressed);
+		updateDisplay();
+	}
+
 }
 
-function updateDisplay(id) {
-	if (display.innerHTML == "0"){
-		display.innerHTML = id;
+function updateDisplay() {
+	if (display.innerHTML == "0") {
+		display.innerHTML = btnPressed.join("");
 	}
 	else {
-		display.innerHTML += id;
+		display.innerHTML = opPressed.join("") + btnPressed.join("");
 	}
 }
 
 function clearDisplay() {
 	display.innerHTML = 0;
-	numPressed = []; // empty array
+	btnPressed = []; 
+	opPressed = [];
 }
 
 function evaluate() {
-	temp_str = numPressed.join(""); 
+	temp_str = opPressed.join("") + btnPressed.join(""); 
 	answer = eval(temp_str);
 	display.innerHTML = answer;
-	numPressed = [String(answer)];
+	btnPressed = [String(answer)];
+	opPressed = [];
 }
 
