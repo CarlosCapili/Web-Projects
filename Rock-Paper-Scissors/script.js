e4092f7aa377695cb34c5cff80cca6726886c79e
CarlosCapili <carloscapili21@gmail.com>
2021-07-22 18:00:23 -0400

    Implementing simple Ui

--- Rock-Paper-Scissors/script.js
+++ Rock-Paper-Scissors/script.js
@@ -2,25 +2,34 @@ const computerChoice = ["rock", "paper", "scissors"];
 const winningCombo = [["rock", "scissors"], ["paper", "rock"], ["scissors", 
 "paper"]];
 
+const playerChoices = document.querySelectorAll('button');
+
 let playerScore = 0;
 let computerScore = 0;
 
+playerChoices.forEach((button) => {
+   
+    button.addEventListener('click', () => {
+        console.log(button.id);
+        console.log(playRound(button.id, computerPlay()));
+    });
+});
+
 function computerPlay () {
-    return computerChoice[Math.floor(Math.random()*3)]
+    return computerChoice[Math.floor(Math.random()*3)];
 }
 
 //Maybe change the return to the winner of the round and have a separate function to update the score and display
 function playRound (playerSelection, computerSelection) {
-    let pSelection = playerSelection.toLowerCase();
     let roundWinner = 0;
   
     //Same choice
-    if (pSelection == computerSelection) {
+    if (playerSelection == computerSelection) {
         roundWinner = 2;
     } else {
         for (let i=0; i<3; i++) {
             let combo = winningCombo[i];
-            if (pSelection == combo[0] && computerSelection == combo[1]) {
+            if (playerSelection == combo[0] && computerSelection == combo[1]) {
                 roundWinner = 1;
             }
         }
@@ -29,27 +38,16 @@ function playRound (playerSelection, computerSelection) {
     let str;
 
     if (roundWinner == 2) {
-       str = "Tie game! " + pSelection + " and " + computerSelection + " cancel out";
+       str = "Tie game! " + playerSelection + " and " + computerSelection + " cancel out";
     } else if (roundWinner == 1){
-        str = "You win! " + pSelection + " beats " + computerSelection;
+        str = "You win! " + playerSelection + " beats " + computerSelection;
         playerScore++;
     } else {
-        str = "You lose! " +computerSelection + " beats " + pSelection;
+        str = "You lose! " +computerSelection + " beats " + playerSelection;
         computerScore++;
     }
-
     return str;
 }
 
-function game() {
-    for (let i=0; i<5; i++) {
-        let selection = prompt("Rock, Paper, or Scissors?");
-        let comp = computerPlay();
-        console.log(playRound(selection, comp));
-        console.log("Player score: "+playerScore);
-        console.log("Computer score: "+computerScore);
-    }
-}
 
-game();
 
