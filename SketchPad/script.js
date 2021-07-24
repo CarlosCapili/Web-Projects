8da0438653d5e9baf21a04b5b277f96c16e5924f
CarlosCapili <carloscapili21@gmail.com>
2021-07-24 18:51:59 -0400

    Cleaned up code and made it more modular

--- SketchPad/script.js
+++ SketchPad/script.js
@@ -1,46 +1,65 @@
 const gridDiv = document.querySelector(".grid");
-
 //Color choices 
 let color = "#000";
-const currentColor = document.querySelector("#currentColor");
-const colorTemplate = document.querySelector("#colorTemplate");
 const colors = ["FFFFFF", "C0C0C0", "808080", "000000", 
                 "FF0000", "800000", "FFFF00", "808000",
                 "00FF00", "008000", "00FFFF", "008080",
                 "0000FF", "000080", "FF00FF", "800080"];
-const titleColor = document.querySelectorAll(".multicolor");
-
 //Tool choices
 let gridLines = 0;
-const toggleGrid = document.querySelector("#toggle-grid");
-const eraser = document.querySelector("#clear-grid");
+const isToggleClicked = document.querySelector("#toggle-grid");
+const isEraserClicked = document.querySelector("#clear-grid");
 
-//Create size of grid
-gridDiv.style.gridTemplateColumns = 'repeat(30, 1fr)';
-gridDiv.style.gridTemplateRows = 'repeat(30, 1fr)';
-//create color palette
-colorTemplate.style.gridTemplateColumns = 'repeat(8, 1fr)';
-colorTemplate.style.gridTemplateRows = 'repeat(2, 1fr)';
+randomColor();
+createGrid();
+createColorTemplate();
+setInterval(randomColor, 2000);
 
 //Create grid cells
-for (let i=0; i<900; i++) { 
-    let cell = document.createElement('div');
-    cell.classList.add('cell');
-    gridDiv.append(cell);
+function createGrid () {
+    gridDiv.style.gridTemplateColumns = 'repeat(30, 1fr)';
+    gridDiv.style.gridTemplateRows = 'repeat(30, 1fr)';
+    for (let i=0; i<900; i++) { 
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        gridDiv.append(cell);
+    }
 }
 const gridCell = document.querySelectorAll(".cell");
 
 //Create color cells
-for (let j=0; j<16; j++) {
-    let color = document.createElement('div');
-    color.classList.add("colors");
-    color.style.backgroundColor = "#"+colors[j];
-    colorTemplate.append(color);
+function createColorTemplate() {
+    const colorTemplate = document.querySelector("#colorTemplate");
+    colorTemplate.style.gridTemplateColumns = 'repeat(8, 1fr)';
+    colorTemplate.style.gridTemplateRows = 'repeat(2, 1fr)';
+    for (let j=0; j<16; j++) {
+        let color = document.createElement('div');
+        color.classList.add("colors");
+        color.style.backgroundColor = "#"+colors[j];
+        colorTemplate.append(color);
+    }
 }
+
 const colorCell = document.querySelectorAll(".colors");
 
-//Event Listeners
-toggleGrid.addEventListener("click", () => {
+function changeColor() {
+    const currentColor = document.querySelector("#currentColor");
+    color = this.style.backgroundColor;
+    currentColor.style.backgroundColor = color;
+}
+
+function fillColor() {
+    this.style.backgroundColor = color;
+}
+
+function randomColor() {
+    const titleColor = document.querySelectorAll(".multicolor");
+    titleColor.forEach((e) => {
+        e.style.color = "#"+colors[Math.floor((Math.random()*(16-4+1))+4)];
+    });
+}
+
+function toggleGrid() {
     if (gridLines == 0) {
         gridCell.forEach((e) => {
             e.style.borderTop = "1px solid black";
@@ -53,34 +72,19 @@ toggleGrid.addEventListener("click", () => {
         });
         gridLines = 0;
     }
-});
+}
 
-eraser.addEventListener("click", () => {
+function clearGrid() {
     gridCell.forEach((e) => {
         e.style.backgroundColor = "#fff";
     });
-});
-
-gridCell.forEach((cell) => {
-    cell.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = color;
-    });
-});
-
-colorCell.forEach((cell) => {
-    cell.addEventListener("click", (e) => {
-        color = e.target.style.backgroundColor;
-        currentColor.style.backgroundColor = color;
-    });
-});
-
-function changeColor() {
-    titleColor.forEach((e) => {
-        e.style.color = "#"+colors[Math.floor((Math.random()*(16-4+1))+4)];
-    });
 }
 
-setInterval(changeColor, 2000);
+//Event Listeners
+isToggleClicked.addEventListener("click", toggleGrid);
+isEraserClicked.addEventListener("click", clearGrid);
+gridCell.forEach(cell => cell.addEventListener("mouseenter", fillColor));
+colorCell.forEach(cell => cell.addEventListener("click", changeColor));
 
 
 
