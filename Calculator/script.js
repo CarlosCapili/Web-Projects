f29069757b6715bef5e3e354d570d82100f5ed0b
CarlosCapili <carloscapili21@gmail.com>
2021-07-26 15:20:13 -0400

    Implemented functionality of calculator

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,89 +1,123 @@
-var button = document.querySelector(".calc-keypad");
-var display = document.getElementById("display");
-var btnPressed = [];
-var opPressed = [];
-var opClicked = 0; //1 means op clicked
+const displayDiv = document.querySelector("#display");
+const keypad = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const del = document.querySelector("#del");
+const clear = document.querySelector("#AC");
+const equalPressed = document.querySelector("#equal");
 
-button.addEventListener('click', btn_clicked);
+let displayValue = "";
+let numArr = [];
+let numArr2 = [];
+let op = ""
+let equal = false;
 
-function btn_clicked() {
-	var id = event.target.id;
-	 
-	if (id == "equal") {
-		evaluate();
-	}
-	else if (id == "AC") {
-		clearDisplay();
-	}
-	else {
-		if (id == "+" || id == "-" || id == "*" || id == "/") {
-			if (opClicked == 1){
-				// does not allow more than 1 operation to be displayed eg. ++
-				// if an operation is already present it will replace it 
-				// eg. before: 12+ after: 12-
-				var lastEle = opPressed[opPressed.length-1];
-				var newStr = lastEle.slice(0,lastEle.length-1) + id;
-				opPressed.pop()
-				opPressed.push(newStr);
-			}
-			else if (btnPressed.length == 0){ 
-				opPressed.push("0" + id);
-			}
-			else {
-				opPressed.push(btnPressed.join("") + id);
-				btnPressed = []; 
-			}
+keypad.forEach(button => button.addEventListener("click", keyPressed));
+operator.forEach(operator => operator.addEventListener("click", operatorPressed));
+del.addEventListener("click", deleteNum);
+clear.addEventListener("click", clearDisplay);
+equalPressed.addEventListener("click", evaluate);
 
-			opClicked = 1;
-			console.log(opPressed);
-		}
-		else {
-			opClicked = 0;
-			btnPressed.push(id);
-			if (id == "0" && btnPressed.length >= 1) {
-				btnPressed.pop(); //prevent leading zeroes from displaying
-			}
-			if (id == "0" && opPressed.length >= 1 && btnPressed.length == 0) {
-				btnPressed.push(id);	
-			}
-			console.log(btnPressed);
-		}
-		updateDisplay();
+function keyPressed() {
+	if (op.length == 0) {
+		numArr.push(this.id);
+		console.log('numArr = '+numArr);
+	} else {
+		numArr2.push(this.id);
+		console.log('numArr2 = '+numArr2);
 	}
+	display();
 }
 
-function updateDisplay() {
-	display.innerHTML = opPressed.join("") + btnPressed.join("");
-	
-	//if an operator is pressed first, then display 0
-	if (display.innerHTML.length == 0){
-		display.innerHTML = 0;
+function operatorPressed() {
+	op = this.id
+	console.log(op);
+}
+
+function display() {
+	if (equal == false) {
+		if (op.length == 0) {
+			if (numArr.length == 0) {
+				displayValue = "0";
+			} else {
+				displayValue = numArr.join('');
+			}
+		} else {
+			if (numArr2.length == 0) {
+				displayValue = "0";
+			} else {
+				displayValue = numArr2.join('');
+			}
+		}
 	}
+	displayDiv.textContent = displayValue;
 }
 
 function clearDisplay() {
-	opClicked = 0;
-	display.innerHTML = 0;
-	btnPressed = []; 
-	opPressed = [];
+	displayValue = "0";
+	numArr = [];
+	numArr2 = [];
+	op = "";
+	display();
+}
+
+function deleteNum() {
+	if (op.length == 0){
+		numArr.pop();
+	} else {
+		numArr2.pop();
+	}
+	display();
 }
 
 function evaluate() {
-	opClicked = 0;
-	if (opPressed.length == 0 && btnPressed.length == 0) {
-		display.innerHTML = 0;
+	a = parseFloat(numArr.join(''));
+	b = parseFloat(numArr2.join(''));
+	let answer;
+	console.log("a = " +a);
+	console.log("b = " +b);
+
+	switch(op) {
+		case "+":
+			answer = add(a,b);
+			break;
+		case "-":
+			answer = subtract(a,b);
+			break;
+		case "*":
+			answer = multiply(a,b);
+			break;
+		case "/":
+			answer = divide(a,b);
+			break;
+		default:
+			console.log("DID NOT PRESS AN OPERATOR!");
 	}
-	else if (btnPressed.length == 0 && opPressed.length >= 1) {
-		btnPressed = [];
-		opPressed =[];
-		display.innerHTML = "Syntax ERROR!";
-	}
-	else {
-		temp_str = opPressed.join("") + btnPressed.join(""); 
-		answer = eval(temp_str);
-		display.innerHTML = answer;
-		btnPressed = [String(answer)];
-		opPressed = [];
-	}
+
+	displayValue = String(answer);
+	equal = true;
+	display();
+	equal = false;
+	// After equals is pressed set operator pressed, and both arrays to empty
+	op = "";
+	numArr = [];
+	numArr2 = [];
 }
 
+
+function add(a,b) {
+	return (a+b);
+}
+
+function subtract(a,b) {
+	return (a-b);
+}
+
+function multiply(a,b) {
+	return (a*b);
+}
+
+function divide(a,b) {
+	return (a/b);
+}
+
+display();
