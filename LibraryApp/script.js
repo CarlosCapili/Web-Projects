8c96b7d9d3ba4d8f553206d25e6ba96d3f608bf9
CarlosCapili <carloscapili21@gmail.com>
2021-07-30 18:22:33 -0400

    Finished general layout

--- LibraryApp/script.js
+++ LibraryApp/script.js
@@ -1,11 +1,15 @@
 let myLibrary = [];
 
+const addClicked = document.querySelector("#add");
+
+addClicked.addEventListener('click', addBookToLibrary);
+
 function Book() {
   // the constructor...
 }
 
 function addBookToLibrary() {
-  
+    
 }
 
 function displayBooks() {
