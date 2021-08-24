f30ca07ec309d10154322462faa65d66cfe522c1
MouthOpenMonkey <59804756+CarlosCapili@users.noreply.github.com>
2021-08-23 22:59:40 -0400

    Update home.js

--- RestaurantPage/src/home.js
+++ RestaurantPage/src/home.js
@@ -1,31 +1,4 @@
-function createNav() {
-    const divBar = document.createElement('div');
-    divBar.classList.add('bar');
-
-    const navDiv = document.createElement('div');
-    navDiv.setAttribute('id', 'nav-bar');
-    const navTag = document.createElement('nav');
-    
-    const homeBtn = document.createElement('button');
-    homeBtn.textContent = 'HOME';
-    const menuBtn = document.createElement('button');
-    menuBtn.textContent = 'MENU';
-    const careersBtn = document.createElement('button');
-    careersBtn.textContent = 'CAREERS';
-    const contactBtn = document.createElement('button');
-    contactBtn.textContent = 'CONTACT';
-
-    navTag.appendChild(homeBtn);
-    navTag.appendChild(menuBtn);
-    navTag.appendChild(careersBtn);
-    navTag.appendChild(contactBtn);
-    navDiv.appendChild(navTag);
-    divBar.appendChild(navDiv)
-
-    return divBar;
-}
-
-function createTitleBar() {
+function createHomepage() {
     const mainDiv = document.createElement('div');
     mainDiv.setAttribute('id', 'about');
 
@@ -47,21 +20,7 @@ function createTitleBar() {
     return mainDiv;
 }
 
-function createFooter() {
-    const footerTag = document.createElement('footer');
-    footerTag.classList.add('bar');
-
-    const pTag = document.createElement('p');
-    pTag.textContent = 'Founded 1957 in Toronto';
-
-    footerTag.appendChild(pTag);
-
-    return footerTag;
-}
-
-export default function loadHomePage() {
+export default function loadHomepage() {
     const main = document.querySelector("#content");
-    main.appendChild(createNav());
-    main.appendChild(createTitleBar());
-    main.appendChild(createFooter());
+    main.appendChild(createHomepage());
 }
