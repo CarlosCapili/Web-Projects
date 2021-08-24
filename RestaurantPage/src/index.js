5c911f0cec67d057630a577c91443f1c0da9d8aa
MouthOpenMonkey <59804756+CarlosCapili@users.noreply.github.com>
2021-08-23 22:59:21 -0400

    Update index.js

--- RestaurantPage/src/index.js
+++ RestaurantPage/src/index.js
@@ -1,3 +1,32 @@
-import loadHomePage from './home';
+import loadMainpage from './main';
+import loadHomepage from './home';
 
-content.appendChild(loadHomePage());
+loadMainpage();
+loadHomepage(); //default load
+
+const tabBtn = document.querySelectorAll(".tabs");
+tabBtn.forEach(e => {
+    e.addEventListener("click", tabClicked);
+});
+
+function tabClicked(e) {
+    let tab = e.target.id;
+    switch(tab) {
+        case "menu":
+            console.log(tab);
+            break;
+        case "contact":
+            console.log(tab);
+            break;
+        case "careers":
+            console.log(tab);
+            break;
+        default:
+            console.log(tab);
+            loadHomepage();
+            break;
+    }
+}
+
+
+
